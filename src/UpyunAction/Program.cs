e63588d49441a2ab479793cc90683bbee279b221
yiyun <yiyungent@gmail.com>
2021-12-10 18:14:33 +0800

    feat: test 1

--- src/UpyunAction/Program.cs
+++ src/UpyunAction/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace UpyunAction // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WriteLine($"Hello World!");
+
+            WriteLine($"args: {string.Join(", ", args)}");
+
+            var envKeys = Environment.GetEnvironmentVariables().Keys;
+            foreach (var key in envKeys)
+            {
+                WriteLine($"Environment: {key}");
+            }
+
+
+            WriteLine($"Environment: INPUT_UPYUN_TOKEN: {Environment.GetEnvironmentVariable("INPUT_UPYUN_TOKEN")}");
+        }
+
+        public static void WriteLine(string message)
+        {
+            Console.WriteLine($"UpyunAction {DateTime.Now}: {message}");
+        }
+
+    }
+}
